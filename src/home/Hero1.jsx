099acf2c4ece49dcae551e014f8c1c62ad75a179
zaheer-web <design.by.zaheer@gmail.com>
2026-05-22 16:24:16 +0530

    first commit

--- src/home/Hero1.jsx
+++ src/home/Hero1.jsx
@@ -119,7 +119,7 @@ export default function Hero1() {
           >
             <Sparkles className="h-3.5 w-3.5 text-[#db3884]" />
 
-            Pink Roof Interior Studio
+            Pinking Roof Interior Studio
           </motion.div>
 
           {/* HEADING */}
